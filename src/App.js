f48e8364830d859c4f852309680ab52579188ea0
yunji <yunjigo92@gmail.com>
2022-12-13 17:32:21 +0900

    Add Dodo list
    - React로 Todo list 구현하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('i run all the time');
-  useEffect(() => console.log('나는 한번만 실행되용'),[]);
-  useEffect(() => {
-    if(keyword !== '' && keyword.length > 5){
-      console.log('나는 키워드가 5단어 이상일 경우 실행되요', keyword);
+  const [toDo, setTodo] = useState('');
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ''){
+      return;
     }
-  },[keyword])
-  useEffect(() => {
-      console.log('나는 카운트가 변경되면 실행되요');
-  },[counter])
-
-  useEffect(() => {
-    console.log('나는 카운트랑 키워드가 변경되면 실행되요');
-  },[counter, keyword])
+    setTodos(currentArray => [toDo, ...currentArray])
+    setTodo('');
+  }
+  console.log(toDos);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder='Search here...'/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={toDo} onChange={onChange} placeholder='write your todo..'/>
+        <button>Add To do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item,index)=> <li key={index}> {item}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
