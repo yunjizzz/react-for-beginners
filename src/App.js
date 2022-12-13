ee776fc9e32472d2bd2cf78469be7e72e626d4af
yunji <yunjigo92@gmail.com>
2022-12-13 17:02:11 +0900

    start react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log('i run all the time');
+  useEffect(() => console.log('나는 한번만 실행되용'),[]);
+  useEffect(() => {
+    if(keyword !== '' && keyword.length > 5){
+      console.log('나는 키워드가 5단어 이상일 경우 실행되요', keyword);
+    }
+  },[keyword])
+  useEffect(() => {
+      console.log('나는 카운트가 변경되면 실행되요');
+  },[counter])
+
+  useEffect(() => {
+    console.log('나는 카운트랑 키워드가 변경되면 실행되요');
+  },[counter, keyword])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder='Search here...'/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
