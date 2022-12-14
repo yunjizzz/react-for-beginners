c2799855676aea07464a1505cffa76200de34fbc
yunji <yunjigo92@gmail.com>
2022-12-14 17:40:35 +0900

    Add movie site
    - TMDB를 통한 TV&Movie 정보 받아서 화면에 보여주기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import {useState} from 'react';
+import { useEffect, useState } from "react";
+import Movie from './Movie';
 
-function App() {
-  const [toDo, setTodo] = useState('');
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ''){
-      return;
-    }
-    setTodos(currentArray => [toDo, ...currentArray])
-    setTodo('');
-  }
-  console.log(toDos);
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={toDo} onChange={onChange} placeholder='write your todo..'/>
-        <button>Add To do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item,index)=> <li key={index}> {item}</li>)}
-      </ul>
-    </div>
-  )
+function App(){
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+   
+    useEffect(() => {
+        async function getMovies(){
+            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=6735a4f592fdecc2695f92ab263f72f4&language=ko-KO&region=KR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
+            const data = await response.json();
+            setMovies(data.results);
+            setLoading(false);
+        }
+        getMovies();
+    },[]);
+    return (
+        <div>
+           {loading ? <div>Loading...</div> 
+           : (
+                <div>
+                    {movies.map((movie) => (
+                        <Movie
+                            poster_path={movie.poster_path}
+                            title={movie.title}
+                            overview={movie.overview}
+                            genre_ids={movie.genre_ids}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
