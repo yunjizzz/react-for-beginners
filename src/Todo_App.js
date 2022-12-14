eccbb0552096d08c41e2af0e1aad34b9d29ee9b3
yunji <yunjigo92@gmail.com>
2022-12-14 12:56:25 +0900

    add coin tracker
    - 코인 정보 가져와서 보여주는 거 추가

---
+++ src/Todo_App.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+
+// Todo list를 위한 App.js
+function Todo_App() {
+  const [toDo, setTodo] = useState('');
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ''){
+      return;
+    }
+    setTodos(currentArray => [toDo, ...currentArray])
+    setTodo('');
+  }
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={toDo} onChange={onChange} placeholder='write your todo..'/>
+        <button>Add To do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item,index)=> <li key={index}> {item}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default Todo_App;
