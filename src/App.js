ded14c9e74c52c895b82ac6543cb9f3d7874fed6
yunji <yunjigo92@gmail.com>
2022-12-15 17:32:55 +0900

    App.js Route 추가
    - App.js에 Route 추가하여 요청 url 별로 분기 되도록 수정

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import { useEffect, useState } from "react";
-import Movie from './Movie';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from './routes/Home'
+import Detail from './routes/Detail'
 
 function App(){
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-   
-    useEffect(() => {
-        async function getMovies(){
-            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=6735a4f592fdecc2695f92ab263f72f4&language=ko-KO&region=KR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
-            const data = await response.json();
-            setMovies(data.results);
-            setLoading(false);
-        }
-        getMovies();
-    },[]);
-    return (
-        <div>
-           {loading ? <div>Loading...</div> 
-           : (
-                <div>
-                    {movies.map((movie) => (
-                        <Movie
-                            poster_path={movie.poster_path}
-                            title={movie.title}
-                            overview={movie.overview}
-                            genre_ids={movie.genre_ids}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+   return (
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+            <Route path="/"  element={<Home/>}/>
+            <Route path="/movie/:id" element={<Detail/>}/>
+            <Route path="/hello" element={<h1>hello</h1>}/>
+        </Routes>
+   </BrowserRouter>);
 }
 
 export default App;
